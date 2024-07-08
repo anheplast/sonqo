5085566eb48db8665f9fbde90264e5642aed4327
anheplast <martinmayanquer01@gmail.com>
2024-07-08 00:01:21 -0500

    Ip cambiada

--- run.py
+++ run.py
@@ -10,9 +10,5 @@ if __name__ == '__main__':
     CORS(app)
     with app.app_context():
         db.create_all()
-<<<<<<< HEAD
     app.run(debug=True, host='192.168.1.13', port=5000)
-=======
-    app.run(debug=True, host='192.168.1.15', port=5000)
->>>>>>> 5de1d6d499c197fb28feda73be2c5563686803e9
     print("Script ejecutado correctamente")
